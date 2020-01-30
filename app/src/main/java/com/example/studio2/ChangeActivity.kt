bac7fa3420c9bb0348e05a2627213b3cea232e7d
Nikash Taskar <nikashtaskar@wustl.edu>
2020-01-30 15:08:21 -0600

    Change balance done

--- app/src/main/java/com/example/studio2/ChangeActivity.kt
+++ app/src/main/java/com/example/studio2/ChangeActivity.kt
@@ -1,7 +1,10 @@
 package com.example.studio2
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_change.*
@@ -12,7 +15,13 @@ class ChangeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change)
+    }
 
+    fun switchBack(view: View){
+        var result = Intent()
+
+        setResult(Activity.RESULT_OK, result)
+        finish()
     }
 
 }
--- app/src/main/java/com/example/studio2/MainActivity.kt
+++ app/src/main/java/com/example/studio2/MainActivity.kt
@@ -1,19 +1,44 @@
 package com.example.studio2
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    enum class RequestInt(code: Int) {
+        CHANGE_BALANCE(1)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    fun changeBalance(view: View){
+        val changeBalIntent = Intent(this, ChangeActivity::class.java)
+
+        var requestInt = RequestInt.CHANGE_BALANCE.ordinal
+
+        startActivityForResult(changeBalIntent, requestInt)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RequestInt.CHANGE_BALANCE.ordinal){
+            if(resultCode == Activity.RESULT_OK){
+                Log.d("DEBUG", "This works")
+            }
+        }
     }
 
 }
